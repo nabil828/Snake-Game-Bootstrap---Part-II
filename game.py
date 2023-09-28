2305a3291afffc337d4a195ea29a2adce00898ea
Nabil M. Al-Rousan <rousani@gmail.com>
2023-09-27 19:27:54 -0700

    Game over if we hit the wall or the snake itself @done

--- game.py
+++ game.py
@@ -8,7 +8,7 @@ class Game:
     def __init__(self):
         pygame.init()
         # Create a window
-        self.screen = pygame.display.set_mode((800, 600))
+        self.screen = pygame.display.set_mode((800, 800))
 
         # Set the title of the window
         pygame.display.set_caption("Snake Game")
@@ -17,12 +17,13 @@ class Game:
         self.screen.fill((255, 255, 255))  # white color
 
         # game objects
-        self.snake = Snake(100, 100, self.screen)
+        self.snake = Snake(40, 40, self.screen)
         self.food = Food(200, 200, self.screen)
 
     def run(self):
         clock = pygame.time.Clock()
         FPS = 10
+        pause = False
 
         # Game loop
         running = True
@@ -33,22 +34,42 @@ class Game:
                 if event.type == pygame.QUIT:
                     running = False
                 if event.type == pygame.KEYDOWN:
-                    if event.key == pygame.K_UP:
-                        self.snake.direction = Direction.UP
-                    elif event.key == pygame.K_DOWN:
-                        self.snake.direction = Direction.DOWN
-                    elif event.key == pygame.K_LEFT:
-                        self.snake.direction = Direction.LEFT
-                    elif event.key == pygame.K_RIGHT:
-                        self.snake.direction = Direction.RIGHT
+                    if not pause:
+                        if event.key == pygame.K_UP:
+                            self.snake.direction = Direction.UP
+                        elif event.key == pygame.K_DOWN:
+                            self.snake.direction = Direction.DOWN
+                        elif event.key == pygame.K_LEFT:
+                            self.snake.direction = Direction.LEFT
+                        elif event.key == pygame.K_RIGHT:
+                            self.snake.direction = Direction.RIGHT
+                    else:
+                        if event.key == pygame.K_SPACE:
+                            pause = False
+                            self.snake = Snake(40, 40, self.screen)
 
-            self.update()
-            self.draw()
+            try:
+                if not pause:
+                    self.update()
+                    self.draw()
+            except Exception as e:
+                pause = True
+                self.game_over()  # to display the text "Game Over"
 
     def update(self):
-        self.snake.update()
+        self.snake.update(self.food)
 
     def draw(self):
         self.screen.fill((255, 255, 255))
         self.snake.draw()
         self.food.draw()
+
+    def game_over(self):
+        # fill
+        self.screen.fill((255, 255, 255))
+        font = pygame.font.SysFont("Arial", 48)
+        text = font.render("Game Over. Hit Space to restart!", True, (0, 0, 0))
+        self.screen.blit(
+            text, (400 - text.get_width() / 2, 400 - text.get_height() / 2)
+        )
+        pygame.display.flip()
--- snake.py
+++ snake.py
@@ -1,3 +1,4 @@
+import random
 import pygame
 
 from direction import Direction
@@ -5,27 +6,123 @@ from direction import Direction
 
 class Snake:
     def __init__(self, x, y, screen):
-        self.x = x
-        self.y = y
+        # self.x = x
+        # self.y = y
         self.screen = screen
         self.direction = Direction.RIGHT
         self.block_size = 40
+        self.segments = [
+            {"x": x, "y": y},
+            {"x": x + self.block_size, "y": y},
+            {"x": x + self.block_size * 2, "y": y},
+            {"x": x + self.block_size * 3, "y": y},
+        ]
+        # the head is the last element in the list
 
     def draw(
         self,
     ):
-        # draw  a rectangle
-        pygame.draw.rect(self.screen, (0, 0, 0), (self.x, self.y, self.block_size, self.block_size))
+        for segment in self.segments:
+            # draw  a rectangle
+            pygame.draw.rect(
+                self.screen,
+                (0, 0, 0),
+                (segment["x"], segment["y"], self.block_size, self.block_size),
+            )
         # Update the display
         pygame.display.flip()
 
-    def update(self):
+    def update(self, food):
         """Update the snake position"""
+
+        # update the body of the snake
+        for i in range(len(self.segments) - 1):
+            self.segments[i]["x"] = self.segments[i + 1]["x"]
+            self.segments[i]["y"] = self.segments[i + 1]["y"]
+
+        # update the head of the snake
         if self.direction == Direction.RIGHT:
-            self.x += self.block_size
+            self.segments[-1]["x"] += self.block_size
         elif self.direction == Direction.LEFT:
-            self.x -= self.block_size
+            self.segments[-1]["x"] -= self.block_size
         elif self.direction == Direction.UP:
-            self.y -= self.block_size
+            self.segments[-1]["y"] -= self.block_size
         elif self.direction == Direction.DOWN:
-            self.y += self.block_size
+            self.segments[-1]["y"] += self.block_size
+
+        # detect collision with food,
+        if self.detect_collision_with_food(food):
+            self.increase_the_size_of_snake_by_one()
+            self.change_the_location_of_the_food(food)
+
+        # detect collision with the wall
+        if self.detect_collision_with_wall():
+            raise "Game over - collision with the wall"
+    
+        # detect collision with itself
+        if self.detect_collision_with_itself():
+            raise "Game over - collision with itself"
+
+    def detect_collision_with_food(self, food):
+        """Detect collision with food"""
+        if (self.segments[-1]["x"] == food.x) and (self.segments[-1]["y"] == food.y):
+            return True
+
+    def increase_the_size_of_snake_by_one(self):
+        """Increase the size of snake by one"""
+        # detect the direction of the snake
+        if self.direction == Direction.RIGHT:
+            self.segments.append(
+                {
+                    "x": self.segments[-1]["x"] + self.block_size,
+                    "y": self.segments[-1]["y"],
+                }
+            )
+        elif self.direction == Direction.LEFT:
+            self.segments.append(
+                {
+                    "x": self.segments[-1]["x"] - self.block_size,
+                    "y": self.segments[-1]["y"],
+                }
+            )
+        elif self.direction == Direction.UP:
+            self.segments.append(
+                {
+                    "x": self.segments[-1]["x"],
+                    "y": self.segments[-1]["y"] - self.block_size,
+                }
+            )
+        elif self.direction == Direction.DOWN:
+            self.segments.append(
+                {
+                    "x": self.segments[-1]["x"],
+                    "y": self.segments[-1]["y"] + self.block_size,
+                }
+            )
+
+    def change_the_location_of_the_food(self, food):
+        """
+        Change the location of the food
+        make sure it is not on the snake
+        """
+        while True:
+            x = (
+                random.randint(0, 19) * self.block_size
+            )  # 0, 40, 80, 120, 160, 200, 240, 280, 320, 360, 400, ..., 760
+            y = random.randint(0, 19) * self.block_size
+            if {"x": x, "y": y} not in self.segments:
+                break
+        food.x = x
+        food.y = y
+    
+    def detect_collision_with_wall(self):
+        """Detect collision with the wall"""
+        if self.segments[-1]["x"] < 0 or self.segments[-1]["x"] > 760:
+            return True
+        elif self.segments[-1]["y"] < 0 or self.segments[-1]["y"] > 760:
+            return True
+
+    def detect_collision_with_itself(self):
+        """Detect collision with itself"""
+        if self.segments[-1] in self.segments[:-1]:
+            return True
