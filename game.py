40cae2fc9613a8c7b682671d11e15b48bd8ca109
Nabil M. Al-Rousan <rousani@gmail.com>
2023-09-27 18:43:48 -0700

    Snake walks! @done

--- game.py
+++ game.py
@@ -1,6 +1,7 @@
 import pygame
 from snake import Snake
 from food import Food
+from direction import Direction
 
 
 class Game:
@@ -20,14 +21,34 @@ class Game:
         self.food = Food(200, 200, self.screen)
 
     def run(self):
+        clock = pygame.time.Clock()
+        FPS = 10
+
         # Game loop
         running = True
         while running:
             # Check for the events
+            clock.tick(FPS)  # 1 frame per second
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     running = False
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_UP:
+                        self.snake.direction = Direction.UP
+                    elif event.key == pygame.K_DOWN:
+                        self.snake.direction = Direction.DOWN
+                    elif event.key == pygame.K_LEFT:
+                        self.snake.direction = Direction.LEFT
+                    elif event.key == pygame.K_RIGHT:
+                        self.snake.direction = Direction.RIGHT
 
-            # Draw the snake
-            self.snake.draw()
-            self.food.draw()
+            self.update()
+            self.draw()
+
+    def update(self):
+        self.snake.update()
+
+    def draw(self):
+        self.screen.fill((255, 255, 255))
+        self.snake.draw()
+        self.food.draw()
--- snake.py
+++ snake.py
@@ -1,15 +1,31 @@
 import pygame
 
+from direction import Direction
+
 
 class Snake:
     def __init__(self, x, y, screen):
         self.x = x
         self.y = y
         self.screen = screen
+        self.direction = Direction.RIGHT
+        self.block_size = 40
 
-    def draw(self, ):
+    def draw(
+        self,
+    ):
         # draw  a rectangle
-        pygame.draw.rect(self.screen, (0, 0, 0), (self.x, self.y, 40, 40))
+        pygame.draw.rect(self.screen, (0, 0, 0), (self.x, self.y, self.block_size, self.block_size))
         # Update the display
         pygame.display.flip()
 
+    def update(self):
+        """Update the snake position"""
+        if self.direction == Direction.RIGHT:
+            self.x += self.block_size
+        elif self.direction == Direction.LEFT:
+            self.x -= self.block_size
+        elif self.direction == Direction.UP:
+            self.y -= self.block_size
+        elif self.direction == Direction.DOWN:
+            self.y += self.block_size
